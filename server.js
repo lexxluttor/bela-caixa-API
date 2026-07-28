ddffdafcb71739d1a418356eb2be994c44d89e5d
lexxluttor <lexxluttor7@gmail.com>
2026-07-28 00:23:46 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -610,7 +610,7 @@ function assinarXmlNFe(xml) {
   const sig = new SignedXml({
     privateKey: cert.privateKeyPem,
     publicCert: cert.certificatePem,
-    canonicalizationAlgorithm: "http://www.w3.org/TR/2001/REC-xml-c14n-20010315",
+    canonicalizationAlgorithm: "http://www.w3.org/2001/10/xml-exc-c14n#",
     signatureAlgorithm: "http://www.w3.org/2000/09/xmldsig#rsa-sha1"
   });
 
@@ -619,7 +619,7 @@ function assinarXmlNFe(xml) {
     uri: "#" + id,
     transforms: [
       "http://www.w3.org/2000/09/xmldsig#enveloped-signature",
-      "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"
+      "http://www.w3.org/2001/10/xml-exc-c14n#"
     ],
     digestAlgorithm: "http://www.w3.org/2000/09/xmldsig#sha1"
   });
@@ -1175,7 +1175,7 @@ function gerarXML(nota) {
 
   return `<?xml version="1.0" encoding="UTF-8"?>
 <NFe xmlns="http://www.portalfiscal.inf.br/nfe">
-  <infNFe xmlns="http://www.portalfiscal.inf.br/nfe" Id="${infNFeId}" versao="4.00">
+  <infNFe Id="${infNFeId}" versao="4.00">
     <ide>
       <cUF>${NFCE_CONFIG.cUF}</cUF>
       <cNF>${esc(nota.cNF)}</cNF>
